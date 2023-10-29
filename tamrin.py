add1d687347096ffab465a211659e4991f7f945b
SIMP <122023665+behnamkordi@users.noreply.github.com>
2023-10-29 10:28:30 +0330

    Add files via upload

---
+++ tamrin.py
@@ -0,0 +1,6 @@
+def name(name, family, age):
+    print("name: "+name)
+    print("family: "+family)
+    print("age: "+age)
+
+name("behnam", "kordi", "21")
---
+++ tamrin7.py
@@ -0,0 +1,40 @@
+
+# tamrin 7
+def CountDown(n,s):
+    if(n==0):
+        print('stop')
+    else:
+        print("n")
+        print(s)
+        CountDown(n-s,s)
+
+        result = CountDown(6,3)
+    
+# tamrin 5
+
+# 'a' and a
+'a' or 2
+
+# tamrin 4
+import math as m 
+m.sqrt(9)
+
+
+
+# tamrin 3
+def baghimande(a,b) : 
+    return (a%b)
+
+result = baghimande(6,8)
+
+
+
+# tamrin 2
+# Armin Fathi & Mahdi Askarpoor
+
+def d_t(t, v, v_r):
+    return (-1 / t) * (v - v_r)
+
+result = d_t(5, 6, 7)
+print(result)
+
