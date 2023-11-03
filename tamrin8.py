5fb71eb08e7a36fc05a90ef1590ccfff5f2e0ee7
SIMP <122023665+behnamkordi@users.noreply.github.com>
2023-11-03 10:45:35 +0330

    Add files via upload

---
+++ tamrin8.py
@@ -0,0 +1,8 @@
+def print_number(num):
+    print("Input number: ", num)
+
+# Get the desired number from the user
+input_number = int(input("Please enter a number: "))
+
+# Call the function with the input number and print it
+print_number(input_number)
