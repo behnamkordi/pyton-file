2c1550f78d23b957420bb25ed14ca995be903266
SIMP <122023665+behnamkordi@users.noreply.github.com>
2023-11-03 10:48:42 +0330

    Update tamrin8.py

--- tamrin8.py
+++ tamrin8.py
@@ -1,8 +1,6 @@
 def print_number(num):
-    print("Input number: ", num)
+    print("Your Input ", num)
 
-# Get the desired number from the user
-input_number = int(input("Please enter a number: "))
+input_number = int(input("enter number: "))
 
-# Call the function with the input number and print it
 print_number(input_number)
