cda2766f7ee8222c06e92bb64120ce6891835e27
SIMP <122023665+behnamkordi@users.noreply.github.com>
2023-11-12 14:40:32 +0330

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,9 @@
+def reverse_string(s):
+    # دو نقطه برای شروع و پایان و منفی یک برای معکوس شروع و پایان رشته
+    reversed_string = s[::-1]
+    print(reversed_string)
+
+inP = input()
+reverse_string(inP)
+
+#Google + خلاقیت خودم
